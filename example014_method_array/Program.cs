bbb683eae511551408daa167b8138d842ef15294
Ilya Plutov <ilya.plutov@gmail.com>
2023-01-15 17:54:35 +0300

    Array projects

---
+++ example014_method_array/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 14, 92, 32, 47, 52, 56, 47, 81, 39 };
+
+int n = array.Length;
+int find = 47;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
